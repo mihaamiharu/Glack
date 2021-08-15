ef024ee481fafdce1bee8b9bb4aeedf67d8f3024
root <root@DESKTOP-A1LA3PN.localdomain>
2021-08-15 15:37:18 +0700

    Add user input validation
    Prevent user to input nil value to username and email

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -12,10 +12,44 @@ describe User do
                     bio: 'au ah gelap'
                 }
 
-                user = User.new(user_data)
+                user = User.new(fill_user)
                 expect(user.username).to eq(fill_user[:username])
                 expect(user.email).to eq(fill_user[:email])
                 expect(user.bio).to eq(fill_user[:bio])
+            end
+        end
+    end
+
+    describe '#valid?' do
+        context 'when given valid params' do
+            it 'should return true' do
+                fill_user = {
+                    username: 'mihaamiharu',
+                    email: 'mihaa@miharu.com'    
+                }
+
+                user = User.new(fill_user)
+                expect(user.valid?).to eq(true)
+            end
+        end
+
+        context 'when given invalid params' do
+            it 'should return false when email is nil' do
+                fill_user = {
+                    username: 'mihaamiharu'   
+                }
+
+                user = User.new(fill_user)
+                expect(user.valid?).to be_falsey
+            end
+
+            it 'should return false when username is nil' do
+                fill_user = {
+                    email: 'mihaa@miharu.com'    
+                }
+
+                user = User.new(fill_user)
+                expect(user.valid?).to be_falsey
             end
         end
     end
