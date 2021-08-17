ae02aec3f3e0bc37b6f90a1ea90e99644bfbe18c
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-17 20:12:13 +0700

    Remove unecessary comment
    Variable name represent to the function so comment isnt't needed anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,22 @@ require_relative '../db/db_connection'
 class User
     attr_accessor :username, :email, :bio
 
-    def initialize(param)
-        @username = param[:username]
-        @email = param[:email]
-        @bio = param[:bio]
-    end
+  def initialize(param)
+      @username = param[:username]
+      @email = param[:email]
+      @bio = param[:bio]
+  end
 
-    #Validation to prevent nil username and email
-    def valid?
-        return false if @username.nil? || @email.nil?
+  def valid?
+      return false if @username.nil? || @email.nil?
 
-        true
-    end
+      true
+  end
 
-    #Save user data
-    def save
-        client = create_db_client
-        sql = "INSERT INTO user (username, email, bio) VALUES ('#{username}', '#{email}', '#{bio}')"
+  def save
+      client = create_db_client
+      sql = "INSERT INTO user (username, email, bio) VALUES ('#{username}', '#{email}', '#{bio}')"
         
-        client.query(sql)
-    end
+      client.query(sql)
+  end
 end
